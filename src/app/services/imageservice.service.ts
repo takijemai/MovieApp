da69bc0de77780d135b44e0aa1e73a9184b992ae
JEMAI TAKIEDDINE <jt800@inlumine.ual.es>
2022-05-31 18:18:14 +0200

    deploy

--- src/app/services/imageservice.service.ts
+++ src/app/services/imageservice.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Photo } from '@capacitor/camera';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ImageserviceService {
+  constructor() {}
 
-  constructor() { }
+  public async readAsBase64(photo: Photo) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath);
+    const blob = await response.blob();
+
+    return (await this.convertBlobToBase64(blob)) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
 }
