340e37160c364b57e110e66b548c73ac42df4406
JEMAI TAKIEDDINE <jt800@inlumine.ual.es>
2022-05-31 12:39:05 +0200

    up

--- src/app/services/peliculaservice.service.ts
+++ src/app/services/peliculaservice.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PeliculaserviceService {
+  url = 'http://www.omdbapi.com/';
+  apiKey = '3c40cb83';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  searchMovie(title: string): Observable<any> {
+    return this.http
+      .get(`${this.url}?s=${encodeURI(title)}}&apikey=${this.apiKey}`)
+      .pipe(map((results) => results['Search']));
+  }
+  getdetails(id: string): Observable<any> {
+    return this.http.get(
+      `${this.url}?i=${encodeURI(id)}&apikey=${this.apiKey}`
+    );
+  }
 }
